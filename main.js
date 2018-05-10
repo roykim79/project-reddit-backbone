95702f180153af99cf65af1b229980c584c34abb
Roy Kim <rykim79@gmail.com>
2018-05-10 07:36:46 -0400

    added model and view

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+var PostModel = Backbone.Model.extend({
+  defaults: {
+    user:  '',
+    text:   ''
+  }
+});
 
+var PostView = Backbone.View.extend({
+  template: Handlebars.compile($('#post-template').html()),
+
+  className: 'post',
+
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+    console.log("this: ", this)
+    console.log("this.el: ", this.el)
+
+    return this;
+  }
+});
+
+$('.add-post').on('click', function () {
+  var text = $('#post-name').val();
+  var user = $('#post-user').val();
+
+  var postModel = new PostModel({ text: text, user: user });
+  var postView = new PostView({ model: postModel });
+
+  $('.posts').append(postView.render().el)
+});
